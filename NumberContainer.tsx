16bee1857fea9bd528c096bd475ad3775446d7b2
Faris Zulfequar <zulfequf@shernet.sheridancollege.ca>
2026-03-13 09:50:04 -0400

    created the default context for the NumberContainer

--- NumberContainer.tsx
+++ NumberContainer.tsx
@@ -1,4 +1,5 @@
 import React, {createContext, ReactNode, useState} from "react";
+import { Text } from 'react-native';
 
 interface NumberContainerType {
   numberMap: Record<number, number>;
@@ -8,4 +9,15 @@ interface NumberContainerType {
 
 interface ProviderProps {
   children: ReactNode;
+}
+
+export const NumberContainer = createContext<NumberContainerType>({
+  numberMap: {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0},
+  resetNumberList: () => {},
+  increaseGeneratedNumberTimes: () => {}
+});
+
+export const NumberContainerProvider = ({children}: ProviderProps) => {
+ return (<Text></Text>)
+
 }
