1d0492e20f1d73da1b4b0a3ecb8a4aad4b742913
Faris Zulfequar <zulfequf@shernet.sheridancollege.ca>
2026-03-13 11:28:54 -0400

    Added stack navigator and created a navigation list type

--- App.tsx
+++ App.tsx
@@ -4,6 +4,16 @@
 
 
 import { GenerationPage } from './GenerationPage.tsx';
+import { StatisticPage } from './StatisticPage.tsx';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+export type NavigationList = {
+  GenerationPage: undefined
+  StatisticPage: undefined
+}
+
+const Stack = createNativeStackNavigator<NavigationList>();
 
 export function App() {
 
